0d616482ba6fd097523ba3b61bbc5f4cf8092d40
s-stepanov <43196053+s-stepanov@users.noreply.github.com>
2021-02-18 16:22:49 +0600

    chore(cors): enable cors for BE (#16)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const originsString: string = configService.get('ALLOWED_ORIGINS');
+
+  app.enableCors({
+    origin: originsString.split(' '),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(configService.get('SERVER_PORT') ?? 3000);
 }
 bootstrap();
