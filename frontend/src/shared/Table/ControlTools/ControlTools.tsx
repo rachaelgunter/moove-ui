30a6e77e2073bca19b3d8bc6626022169ecd1e6a
Daria Kurmaeva <daria_kurmaeva@lineate.com>
2021-03-03 16:45:09 +0600

    feat: add icon to Show More and Show Less buttons

--- frontend/src/shared/Table/ControlTools/ControlTools.tsx
+++ frontend/src/shared/Table/ControlTools/ControlTools.tsx
@@ -1,5 +1,9 @@
 import React, { FC } from 'react';
 import { TableRow } from '@material-ui/core';
+import {
+  ExpandMore as ExpandMoreIcon,
+  ExpandLess as ExpandLessIcon,
+} from '@material-ui/icons';
 
 import TableCell from '../TableCell';
 import Button from '../Button';
@@ -20,8 +24,14 @@ const ControlTools: FC<ShowMoreButtonProps> = ({
 }: ShowMoreButtonProps) => (
   <TableRow key="table-control-row">
     <TableCell align="center" colSpan={colSpan}>
-      <Button onClick={onShowMoreClick}>{SHOW_MORE_BUTTON_TITLE}</Button>
-      <Button onClick={onShowLessClick}>{SHOW_LESS_BUTTON_TITLE}</Button>
+      <Button onClick={onShowMoreClick}>
+        {SHOW_MORE_BUTTON_TITLE}
+        <ExpandMoreIcon />
+      </Button>
+      <Button onClick={onShowLessClick}>
+        {SHOW_LESS_BUTTON_TITLE}
+        <ExpandLessIcon />
+      </Button>
     </TableCell>
   </TableRow>
 );
