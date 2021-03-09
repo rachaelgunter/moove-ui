1084081abe80b57008d4928d8e927bf2024bdf1b
Sergey Stepanov <sstepanov@lineate.com>
2021-03-09 13:43:34 +0600

    feat: [UU-44] create column details modal

--- frontend/src/data-analysis/Columns/ColumnsRow/ColumnsRow.tsx
+++ frontend/src/data-analysis/Columns/ColumnsRow/ColumnsRow.tsx
@@ -1,9 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { TableRow, Theme, makeStyles, Button } from '@material-ui/core';
 
 import { ColumnModel } from 'src/data-analysis/types';
 import { TableCell } from 'src/shared/Table';
 import { ReactComponent as MagnifierIcon } from 'src/assets/icons/magnifier.svg';
+import DetailedColumnView from 'src/data-analysis/DetailedColumnView/DetailedColumnView';
 
 const useStyles = makeStyles((theme: Theme) => ({
   cell: {
@@ -32,6 +33,17 @@ const ColumnsRow: FC<ColumnsRowsProps> = ({
   columnModel,
 }: ColumnsRowsProps) => {
   const classes = useStyles();
+  const [isDetailedColumnViewOpened, setDetailedColumnViewOpened] = useState(
+    false,
+  );
+
+  const openDetailedColumnView = () => {
+    setDetailedColumnViewOpened(true);
+  };
+
+  const closeDetailedColumnView = () => {
+    setDetailedColumnViewOpened(false);
+  };
 
   return (
     <TableRow key={columnModel.name} data-testid="columns-table-row">
@@ -44,10 +56,15 @@ const ColumnsRow: FC<ColumnsRowsProps> = ({
         <Button
           startIcon={<MagnifierIcon />}
           className={classes.rowActionButton}
+          onClick={openDetailedColumnView}
         >
           View
         </Button>
       </TableCell>
+      <DetailedColumnView
+        open={isDetailedColumnViewOpened}
+        onClose={closeDetailedColumnView}
+      />
     </TableRow>
   );
 };
