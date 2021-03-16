97e966b39ab35fd5d181aff6a5c0f0f63ec86be1
Sergey Stepanov <sstepanov@lineate.com>
2021-03-16 18:17:31 +0600

    feat: [UU-44] loader

--- frontend/src/data-analysis/DetailedColumnView/ColumnViewMap.tsx
+++ frontend/src/data-analysis/DetailedColumnView/ColumnViewMap.tsx
@@ -1,10 +1,41 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { FC } from 'react';
 import { useQuery } from '@apollo/client';
-import { CircularProgress } from '@material-ui/core';
+import { CircularProgress, makeStyles } from '@material-ui/core';
 import AutoSizer from 'react-virtualized-auto-sizer';
 import { KEPLER_DATA_QUERY, KEPLER_STRUCTURE_QUERY } from '../queries';
 import KeplerWrapper from './KeplerWrapper';
+
+const useStyles = makeStyles(() => ({
+  spinnerContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    flex: '1 1 auto',
+    display: 'flex',
+  },
+  keplerInstanceContainer: {
+    flex: '1 1 auto',
+  },
+}));
+
+const getData = (rows: any, columns: any): any => {
+  const { tableData } = rows;
+  const { tableInfo } = columns;
+
+  return {
+    info: {
+      label: 'Analysis results',
+      id: 'analysis_data',
+    },
+    data: {
+      fields: tableInfo.schema.fields,
+      rows: tableData.rows.map((row: { f: any[] }) =>
+        row.f.map((field: { v: any }) => field.v),
+      ),
+    },
+  };
+};
 
 const ColumnViewMap: FC = () => {
   const projectId = 'moove-platform-testing-data';
@@ -21,33 +52,27 @@ const ColumnViewMap: FC = () => {
     },
   );
 
+  const classes = useStyles();
+
   if (rowsLoading || columnsLoading) {
-    return <CircularProgress />;
+    return (
+      <div className={classes.keplerInstanceContainer}>
+        <div className={classes.spinnerContainer}>
+          <CircularProgress />
+        </div>
+      </div>
+    );
   }
 
-  const { tableData } = rows;
-  const { tableInfo } = columns;
-
-  const data = {
-    info: {
-      label: 'Analysis results',
-      id: 'analysis_data',
-    },
-    data: {
-      fields: tableInfo.schema.fields,
-      rows: tableData.rows.map((row: { f: any[] }) =>
-        row.f.map((field: { v: any }) => field.v),
-      ),
-    },
-  };
-
-  return rowsLoading || columnsLoading ? (
-    <CircularProgress />
-  ) : (
-    <div style={{ flex: '1 1 auto' }}>
+  return (
+    <div className={classes.keplerInstanceContainer}>
       <AutoSizer>
         {({ width, height }) => (
-          <KeplerWrapper data={data} width={width} height={height} />
+          <KeplerWrapper
+            data={getData(rows, columns)}
+            width={width}
+            height={height}
+          />
         )}
       </AutoSizer>
     </div>
