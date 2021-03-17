0fa0e3fd76e1b1d1147754eef8f9c8980b9266d6
Sergey Stepanov <sstepanov@lineate.com>
2021-03-17 14:37:54 +0600

    feat: [UU-44] hide sidebar by default

--- frontend/src/kepler/store.ts
+++ frontend/src/kepler/store.ts
@@ -2,9 +2,18 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import keplerGlReducer from 'kepler.gl/reducers';
 import { enhanceReduxMiddleware } from 'kepler.gl/middleware';
 
+const customizedKeplerGlReducer = keplerGlReducer.initialState({
+  uiState: {
+    // hide side panel when mounted
+    activeSidePanel: null,
+    // hide all modals whtn mounted
+    currentModal: null,
+  },
+});
+
 const initialState = {};
 const reducer = combineReducers({
-  keplerGl: keplerGlReducer,
+  keplerGl: customizedKeplerGlReducer,
 });
 const middlewares = enhanceReduxMiddleware([]);
 const enhancers = [applyMiddleware(...middlewares)];
