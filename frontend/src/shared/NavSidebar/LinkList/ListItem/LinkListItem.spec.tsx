27f704780a1f808d726bc9e7a49d3efcc18b50e0
Konstantin Artushkevich <kartushkevich@lineate.com>
2021-03-22 21:16:58 +0600

    chore: add tooltip for unavailable links in nav bar

--- frontend/src/shared/NavSidebar/LinkList/ListItem/LinkListItem.spec.tsx
+++ frontend/src/shared/NavSidebar/LinkList/ListItem/LinkListItem.spec.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Link } from 'react-router-dom';
-import { render, RenderResult } from '@testing-library/react';
+import {
+  screen,
+  fireEvent,
+  render,
+  waitFor,
+  RenderResult,
+} from '@testing-library/react';
 import { SvgIcon } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 
@@ -10,7 +16,10 @@ import { LinkListItem } from 'src/shared/NavSidebar/LinkList/ListItem/index';
 import theme from 'src/app/styles';
 
 const testLabel = 'test label';
-const createWrapper = (params?: Partial<NavSidebarContextData>): RenderResult =>
+const createWrapper = (
+  params?: Partial<NavSidebarContextData>,
+  props: { disabled?: boolean } = {},
+): RenderResult =>
   render(
     <ThemeProvider theme={theme}>
       <BrowserRouter>
@@ -20,6 +29,7 @@ const createWrapper = (params?: Partial<NavSidebarContextData>): RenderResult =>
             to="/"
             label={testLabel}
             Icon={SvgIcon}
+            {...props}
           />
         </NavSidebarMock>
       </BrowserRouter>
@@ -33,6 +43,19 @@ describe('LinkListItem', () => {
       const tooltip = wrapper.getByTestId('listItem-tooltip');
       expect(tooltip.title).toEqual('');
     });
+
+    describe('disabled item', () => {
+      it('should have text with link', async () => {
+        const wrapper = createWrapper({ isExpanded: true }, { disabled: true });
+        const tooltip = wrapper.getByTestId('listItem-tooltip');
+
+        fireEvent.mouseOver(tooltip);
+
+        await waitFor(() =>
+          expect(screen.getByText('http://moove.ai')).toBeInTheDocument(),
+        );
+      });
+    });
   });
 
   describe('sidebar is collapsed', () => {
@@ -40,6 +63,22 @@ describe('LinkListItem', () => {
       const wrapper = createWrapper({ isExpanded: false });
       const tooltip = wrapper.getByTestId('listItem-tooltip');
       expect(tooltip.title).toEqual(testLabel);
+    });
+
+    describe('disabled item', () => {
+      it('should have text with link', async () => {
+        const wrapper = createWrapper(
+          { isExpanded: false },
+          { disabled: true },
+        );
+        const tooltip = wrapper.getByTestId('listItem-tooltip');
+
+        fireEvent.mouseOver(tooltip);
+
+        await waitFor(() =>
+          expect(screen.getByText('http://moove.ai')).toBeInTheDocument(),
+        );
+      });
     });
   });
 });
