7bf4365f8c0dddddd0742bd10be21772add04210
Daria Kurmaeva <daria_kurmaeva@lineate.com>
2021-04-08 14:14:27 +0600

    refactor: move to own component AlertDialog

--- frontend/src/data-analysis/DatasetDetails/DatasetDetailsHeader/Archiver/Archiver.tsx
+++ frontend/src/data-analysis/DatasetDetails/DatasetDetailsHeader/Archiver/Archiver.tsx
@@ -1,39 +1,14 @@
 import React, { FC, useState } from 'react';
-import {
-  Button,
-  Link,
-  Dialog,
-  DialogTitle,
-  DialogActions,
-  DialogContent,
-  Theme,
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/styles';
+import { Link, makeStyles } from '@material-ui/core';
 
-import { FontFamily } from 'src/app/styles/fonts';
 import Typography from 'src/shared/Typography';
+import AlertDialog from 'src/shared/AlertDialog';
 
-const useStyles = makeStyles((theme: Theme) => {
-  return {
-    link: {
-      textAlign: 'left',
-      width: '100%',
-    },
-    dialogTitle: {
-      fontWeight: 400,
-      fontSize: 18,
-    },
-    dialogButton: {
-      fontFamily: FontFamily.ROBOTO,
-      color: theme.palette.text.secondary,
-      height: '36px',
-      letterSpacing: '1.25px',
-      marginLeft: theme.spacing(1),
-    },
-    primaryButton: {
-      color: '#e57373',
-    },
-  };
+const useStyles = makeStyles({
+  link: {
+    textAlign: 'left',
+    width: '100%',
+  },
 });
 
 const Archiver: FC = () => {
@@ -46,7 +21,7 @@ const Archiver: FC = () => {
   };
 
   const onCancel = () => {
-    console.warn('cencal');
+    console.warn('cancel');
     setOpen(false);
   };
 
@@ -62,40 +37,14 @@ const Archiver: FC = () => {
           Archive
         </Typography>
       </Link>
-      {/* <DialogWrapper /> */}
-      <Dialog
+      <AlertDialog
         open={open}
+        title="You are about to delete this dataset."
+        message="This action cannot be undone. Do you want to proceed?"
+        actionButtonTitle="Delete"
+        onAction={onDelete}
         onClose={onCancel}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          <Typography
-            color="textPrimary"
-            fontFamily={FontFamily.ROBOTO}
-            className={classes.dialogTitle}
-          >
-            You are about to delete this dataset.
-          </Typography>
-        </DialogTitle>
-        <DialogContent>
-          <Typography color="textSecondary" fontFamily={FontFamily.ROBOTO}>
-            This action cannot be undone. Do you want to proceed?
-          </Typography>
-        </DialogContent>
-        <DialogActions>
-          <Button classes={{ root: classes.dialogButton }} onClick={onCancel}>
-            Canсel
-          </Button>
-          <Button
-            classes={{ root: classes.dialogButton }}
-            className={classes.primaryButton}
-            onClick={onDelete}
-          >
-            Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
+      />
     </>
   );
 };
---
+++ frontend/src/shared/AlertDialog/AlertDialog.tsx
@@ -0,0 +1,96 @@
+import React, { FC } from 'react';
+import {
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  Theme,
+  Button,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+import { FontFamily } from 'src/app/styles/fonts';
+import Typography from 'src/shared/Typography';
+
+const useStyles = makeStyles((theme: Theme) => {
+  return {
+    link: {
+      textAlign: 'left',
+      width: '100%',
+    },
+    dialogTitle: {
+      fontWeight: 400,
+      fontSize: 18,
+    },
+    dialogButton: {
+      fontFamily: FontFamily.ROBOTO,
+      color: theme.palette.text.secondary,
+      height: '36px',
+      letterSpacing: '1.25px',
+      marginLeft: theme.spacing(1),
+    },
+    primaryButton: {
+      color: '#e57373',
+    },
+  };
+});
+
+interface AlertDialogProps {
+  open: boolean;
+  title: string;
+  message: string;
+  onClose: () => void;
+  onAction: () => void;
+  actionButtonTitle: string;
+}
+
+const AlertDialog: FC<AlertDialogProps> = ({
+  open,
+  onClose,
+  title,
+  message,
+  onAction,
+  actionButtonTitle,
+}: AlertDialogProps) => {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Dialog
+        open={open}
+        onClose={onClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          <Typography
+            color="textPrimary"
+            fontFamily={FontFamily.ROBOTO}
+            className={classes.dialogTitle}
+          >
+            {title}
+          </Typography>
+        </DialogTitle>
+        <DialogContent>
+          <Typography color="textSecondary" fontFamily={FontFamily.ROBOTO}>
+            {message}
+          </Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button classes={{ root: classes.dialogButton }} onClick={onClose}>
+            Canсel
+          </Button>
+          <Button
+            classes={{ root: classes.dialogButton }}
+            className={classes.primaryButton}
+            onClick={onAction}
+          >
+            {actionButtonTitle}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default AlertDialog;
