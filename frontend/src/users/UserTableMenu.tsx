0980932cb3942ae684b85d50c5bb7e39e1b34417
Daria Kurmaeva <daria_kurmaeva@lineate.com>
2021-04-25 14:20:08 +0600

    feat: add user menu item for delete control

---
+++ frontend/src/users/UserTableMenu.tsx
@@ -0,0 +1,99 @@
+import React, { FC, useState } from 'react';
+import {
+  IconButton,
+  Theme,
+  makeStyles,
+  Menu,
+  MenuItem,
+  Link,
+  Typography,
+} from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import { User } from 'src/auth/UserProvider';
+import { FontFamily } from 'src/app/styles/fonts';
+
+interface UserTableMenuProps {
+  user: User;
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  rowActionButton: {
+    color: '#fff',
+    padding: 0,
+  },
+  link: {
+    textAlign: 'left',
+    width: '100%',
+  },
+  actionsMenu: {
+    width: 140,
+  },
+  actionsMenuItem: {
+    fontFamily: FontFamily.ROBOTO,
+    '& > a, & > button': {
+      color: theme.palette.text.primary,
+      '&:hover': {
+        textDecoration: 'none',
+      },
+    },
+    '& > button p': {
+      fontFamily: FontFamily.ROBOTO,
+    },
+  },
+  actionsMenuButton: {
+    color: '#fff',
+    marginRight: '15px',
+  },
+}));
+
+const UserTableMenu: FC<UserTableMenuProps> = ({
+  user,
+}: UserTableMenuProps) => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const onMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const onMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  console.warn(user);
+
+  return (
+    <>
+      <IconButton className={classes.rowActionButton} onClick={onMenuOpen}>
+        <MoreVertIcon />
+      </IconButton>
+      <Menu
+        id="simple-menu"
+        classes={{ list: classes.actionsMenu }}
+        anchorEl={anchorEl}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={Boolean(anchorEl)}
+        onClose={onMenuClose}
+      >
+        <MenuItem className={classes.actionsMenuItem}>
+          <Link className={classes.link} component="button">
+            <Typography color="textPrimary" variant="body1">
+              Delete
+            </Typography>
+          </Link>
+        </MenuItem>
+      </Menu>
+    </>
+  );
+};
+
+export default UserTableMenu;
